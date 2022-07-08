31ed15da1bdff94ce68e85362e25e41f7496e28a
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-07-08 15:18:43 +0100

    add post handler

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const messages = [
     added: new Date(),
   },
   {
-    text: "Hello World!",
+    text: "Post any Message... don't worry, I dont keep receipts",
     user: "Charles Xavier",
     added: new Date(),
   },
@@ -18,5 +18,15 @@ const messages = [
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Message Board", messages: messages });
 });
-
+router.get("/new", (req, res) => {
+  res.render("form", { title: "Mini Message Board", messages: messages });
+});
+router.post("/new", (req, res) => {
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.messageUser,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
 module.exports = router;
